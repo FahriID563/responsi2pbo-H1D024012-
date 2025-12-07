703cfd98784e6359f4e2efce8f01c6c72b09dc72
Mhd Fahri Irfandi Dewantara <fahriirfandi564@gmail.com>
2025-12-07 20:14:03 +0700

    Update mesinTraktor.java

--- pert5/mesinTraktor.java
+++ pert5/mesinTraktor.java
@@ -1,27 +1,23 @@
 class mesinTraktor extends defaultMesin {
-    // Variabel khusus mesin traktor (kapasitasTarik)
-
-    // Constructor
+    double kapasitasTarik;
     mesinTraktor(String nama, int hp, double tarik) {
         super(nama, hp);
+        this.kapasitasTarik = tarik;
     }
-
-    @Override
-    void tampilInfo() {
-        // Override info mesin traktor
+    
+    @Override void tampilInfo() {
+        System.out.println("Mesin Traktor " + namaMesin + " | Tarik: " + kapasitasTarik + " ton | Tenaga: " + tenagaHP + " HP");
     }
-
+    
     @Override
     double nilaiPerforma() {
-        // Override performa traktor
+        return (tenagaHP * 0.9) + (kapasitasTarik * 10); 
     }
-
-    @Override
+    
+    @Override 
     String kategoriMesin() {
-        // Override kategori traktor
+        return "Mesin Traktor";
     }
-
-    void suaraMesin() {
-        // Suara traktor
-    }
+    void suaraMesin() { 
+        System.out.println("GGGRRRR! Hidup mesinnn!"); }
 }
