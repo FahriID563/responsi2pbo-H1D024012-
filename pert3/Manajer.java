87275278d2badf62b0f0be13bba678bfc3e3e8fa
Mhd Fahri Irfandi Dewantara <fahriirfandi564@gmail.com>
2025-12-07 20:00:25 +0700

    Implement Manajer class extending Karyawan

--- pert3/Manajer.java
+++ pert3/Manajer.java
@@ -1,16 +1,19 @@
-// Gunakan keyword untuk mewarisi class Karyawan
-class Manajer ... {
-    // Deklarasikan variabel tambahan khusus Manajer (tunjangan)
-    
-    
-    // Constructor
-    // Tips: Gunakan 'super' untuk memanggil constructor parent
-    
-    
-    // Method Override tampilInfo
-    // Tips: Tampilkan info dasar, lalu tambahkan info tunjangan dan total gaji
+class Manajer extends Karyawan {
+    // Variabel tambahan
+    private double tunjangan;
+
+    // Constructor menggunakan super
+    public Manajer(String nama, double gajiPokok, double tunjangan) {
+        super(nama, gajiPokok);
+        this.tunjangan = tunjangan;
+    }
+
+    // Method Override
     @Override
     void tampilInfo() {
-        
+        // Tampilkan info dasar, lalu tambahkan info tunjangan
+        System.out.print("Nama: " + nama + " | Gaji Pokok: Rp " + gajiPokok);
+        System.out.println(" | Tunjangan: Rp " + tunjangan);
+        System.out.println("Total Pendapatan: Rp " + (gajiPokok + tunjangan));
     }
 }
