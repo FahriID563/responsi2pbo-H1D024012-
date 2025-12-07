d815a4732ad860313a2463dea5c5f4210bcf2032
Mhd Fahri Irfandi Dewantara <fahriirfandi564@gmail.com>
2025-12-07 20:00:50 +0700

    Update UjiKaryawan.java

--- pert3/UjiKaryawan.java
+++ pert3/UjiKaryawan.java
@@ -1,22 +1,18 @@
 class UjiKaryawan {
     public static void main(String[] args) {
-        // Tampilkan Header
         System.out.println("=== DATA KARYAWAN TECHMAJU ===");
 
-        // Buat objek Karyawan biasa (Budi)
-        
-        
-        // Tampilkan info Karyawan biasa
+        // Objek Karyawan biasa
+        Karyawan karyawan = new Karyawan("Budi Santoso", 4000000);
         System.out.println("Status: Karyawan Biasa");
-        
-        
-        System.out.println(); // Baris baru/jarak
+        karyawan.tampilInfo();
+        System.out.println(); // Baris baru
 
-        // Buat objek Manajer (Siti)
-        
-        
-        // Tampilkan info Manajer
+        System.out.println();
+
+        // Objek Manajer
+        Manajer manajer = new Manajer("Siti Aminah", 6000000, 2500000);
         System.out.println("Status: Manajer");
-        
+        manajer.tampilInfo();
     }
 }
