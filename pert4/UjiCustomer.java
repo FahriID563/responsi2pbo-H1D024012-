77d801b8e2acbfa8cbea598a70c16e3d06859447
Mhd Fahri Irfandi Dewantara <fahriirfandi564@gmail.com>
2025-12-07 20:04:32 +0700

    Update UjiCustomer.java

--- pert4/UjiCustomer.java
+++ pert4/UjiCustomer.java
@@ -1,14 +1,21 @@
 public class UjiCustomer {
     public static void main(String[] args) {
+        // Buat object Customer
+        Customer c = new Customer("Budi Santoso", "CST-001", 500000);
 
-        // TODO: Buat object Customer
+        // Buat object Member
+        Member m = new Member("Siti Aminah", "MBR-110", 1250000, 240, "Gold");
 
-        // TODO: Buat object Member
+        // Tampilkan judul
+        System.out.println("=== DATA CUSTOMER BELANJAKU ===");
 
-        // TODO: Tampilkan judul data pelanggan
+        // Info Customer Biasa
+        System.out.println("Status: Customer Biasa");
+        c.tampilkanInfo();
+        System.out.println();
 
-        // TODO: Tampilkan info untuk Customer biasa
-
-        // TODO: Tampilkan info untuk pelanggan Member
+        // Info Member
+        System.out.println("Status: Member");
+        m.tampilkanInfo();
     }
 }
